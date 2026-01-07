793dabd4747df11b79069b11644c4e27de749bd7
Vertax42 <1007644658@qq.com>
2026-01-07 11:40:14 +0800

    Update training configurations for Pi05 and Xense Flare
    - Renamed and modified training configurations for clarity and specificity, including updates to model parameters and dataset references.
    - Enhanced the `TrainConfig` for Xense Flare with new data configurations and additional model parameters to support improved training capabilities.
    - Adjusted batch size and number of training steps for better performance in the training pipeline.

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1377,53 +1377,6 @@ _CONFIGS = [
         fsdp_devices=1,  # refer line 359
     ),
     TrainConfig(
-        name="pi05_base_full_test",
-        model=pi0_config.Pi0Config(
-            # paligemma_variant="gemma_2b_lora",
-            # action_expert_variant="gemma_300m_lora",
-            pi05=True,
-        ),
-        data=LeRobotAlohaDataConfig(
-            repo_id="Vertax/xense_bi_arx5_pick_and_place_cube",  # your datasets repo_id
-            assets=AssetsConfig(
-                assets_dir="gs://openpi-assets/checkpoints/pi05_base/assets",
-                asset_id="trossen",
-            ),
-            adapt_to_pi=False,
-            adapt_to_arx5=False,
-            repack_transforms=_transforms.Group(
-                inputs=[
-                    _transforms.RepackTransform(
-                        {
-                            "images": {
-                                "cam_high": "observation.images.head",
-                                "cam_left_wrist": "observation.images.left_wrist",
-                                "cam_right_wrist": "observation.images.right_wrist",
-                            },
-                            "state": "observation.state",
-                            "actions": "action",
-                            "prompt": "prompt",
-                        }
-                    )
-                ]
-            ),
-            base_config=DataConfig(
-                prompt_from_task=True,  # Set to True for prompt by task_name
-            ),
-        ),
-        freeze_filter=pi0_config.Pi0Config(
-            pi05=True,
-            # paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"
-        ).get_freeze_filter(),
-        batch_size=4,  # the total batch_size not pre_gpu batch_size
-        weight_loader=weight_loaders.CheckpointWeightLoader(
-            "gs://openpi-assets/checkpoints/pi05_base/params"
-        ),
-        num_train_steps=20_000,
-        num_workers=1,  # default 2
-        fsdp_devices=1,  # refer line 359
-    ),
-    TrainConfig(
         name="pi05_base_arx5_lora_pick_and_place_chips",
         model=pi0_config.Pi0Config(
             paligemma_variant="gemma_2b_lora",
@@ -1469,6 +1422,51 @@ _CONFIGS = [
     #
     # Fine-tuning XenseFlare configs.
     #
+    TrainConfig(
+        name="pi05_base_xense_flare_pick_and_place_cube",
+        model=pi0_config.Pi0Config(
+            paligemma_variant="gemma_2b_lora",
+            action_expert_variant="gemma_300m_lora",
+            pi05=True,
+            enable_training_time_rtc=True,
+            max_delay=10,
+        ),
+        data=LeRobotXenseFlareDataConfig(
+            repo_id="Vertax/xense_flare_pick_and_place_cube_20260105",  # your datasets repo_id
+            gripper_first=True,
+            use_delta_cartesian_actions=True,
+            default_prompt="pick up cubes in rgb order from the table and place them in the blue box",
+            repack_transforms=_transforms.Group(
+                inputs=[
+                    _transforms.RepackTransform(
+                        {
+                            "images": {
+                                "observation/wrist_image_left": "observation.images.wrist_cam"
+                            },
+                            "state": "observation.state",
+                            "actions": "action",
+                            "prompt": "task",
+                        }
+                    )
+                ]
+            ),
+            base_config=DataConfig(
+                prompt_from_task=True,  # Set to True for prompt by task_name
+            ),
+        ),
+        freeze_filter=pi0_config.Pi0Config(
+            pi05=True,
+            paligemma_variant="gemma_2b_lora",
+            action_expert_variant="gemma_300m_lora",
+        ).get_freeze_filter(),
+        batch_size=64,  # the total batch_size not pre_gpu batch_size
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "gs://openpi-assets/checkpoints/pi05_base/params"
+        ),
+        num_train_steps=40_000,
+        num_workers=1,  # default 2
+        fsdp_devices=1,  # refer line 359
+    ),
     TrainConfig(
         name="pi05_base_xense_flare_open_lock",
         model=pi0_config.Pi0Config(
